321448d385257d673cb9a93ec77c1d9f2930ad95
Zach Bauer <azbauer8@gmail.com>
2023-11-19 17:12:16 -0500

    added Uses to projects section

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -57,6 +57,27 @@ export default function Home() {
               </p>
             </div>
           </div>
+          <div className="relative flex flex-col rounded-lg border border-zinc-200 dark:border-zinc-800">
+            <div className="absolute right-3 top-5">
+              <a href="https://github.com/azbauer8/Uses">
+                <FaGithub className="font-default mr-2 h-6 w-6" />
+              </a>
+            </div>
+            <div className="p-5">
+              <h3 className="font-pop mb-2 font-semibold">Uses</h3>
+              <p className="leading-relaxed">
+                The apps and tools I'm using to get stuff done. May be a little
+                outdated, I'm looking to find a better workflow for maintaining
+                this.
+              </p>
+              <a
+                href="https://uses.byzach.dev"
+                className="focusable mt-4 flex cursor-pointer items-center justify-center rounded-md bg-zinc-200 px-2.5 py-2 font-medium  hover:bg-zinc-300 dark:bg-zinc-500 dark:hover:bg-zinc-600"
+              >
+                Check it out
+              </a>
+            </div>
+          </div>
         </div>
       </div>
     </div>
