aa8023bd80948e362610fbd3e860c4777834ac24
Zach Bauer <azbauer8@gmail.com>
2023-11-25 18:38:40 -0500

    Recreating with Next.js

---
+++ app/layout.tsx
@@ -0,0 +1,35 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import { Providers } from "./providers";
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Zach Bauer",
+  description: `Zach Bauer's Personal Website`,
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <body className={inter.className}>
+        <Providers>
+          <div className="mx-auto mb-10 max-w-3xl space-y-5">
+            <Header />
+            <div className="m-5 space-y-8 md:m-0">
+              <main>{children}</main>
+              <Footer />
+            </div>
+          </div>
+        </Providers>
+      </body>
+    </html>
+  );
+}
---
+++ tailwind.config.ts
@@ -0,0 +1,32 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  darkMode: ["class"],
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
+    },
+    extend: {
+      colors: {
+        zinc: {
+          150: "#bbbbc0",
+          450: "#898992",
+        },
+      },
+      cursor: {
+        birds: "url(/eagles.png), pointer",
+      },
+    },
+  },
+  plugins: [],
+};
+export default config;
