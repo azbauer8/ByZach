f46374da6e8bb56f6b99fe2fd2a6040dcb230bb0
Zach Bauer <azbauer8@gmail.com>
2023-11-26 15:18:26 -0500

    lastfm shows how long ago if not playing now

--- components/LastfmCard.tsx
+++ components/LastfmCard.tsx
@@ -1,6 +1,7 @@
 import fetchLastFm from "@/lib/fetchLastFm";
 import { useQuery } from "@tanstack/react-query";
 import Image from "next/image";
+import { PiWaveformBold } from "react-icons/pi";
 
 const LastFmCard = () => {
   const { isSuccess, isLoading, data } = useQuery({
@@ -13,6 +14,10 @@ const LastFmCard = () => {
   if (isSuccess) {
     const latestTrack = data.recenttracks.track[0];
     console.log(latestTrack);
+    let playingWhen;
+    latestTrack["@attr"]?.nowplaying === undefined
+      ? (playingWhen = getTimeDiff(latestTrack.date["#text"]))
+      : (playingWhen = "Now Playing");
     return (
       <a
         className="flex flex-row max-w-xl rounded-lg space-x-5 ring-offset-4 transition hover:opacity-60 focus:ring-red-500/40 dark:ring-offset-zinc-900 dark:focus:ring-red-400/40 w-fit"
@@ -26,9 +31,10 @@ const LastFmCard = () => {
           className="rounded-lg"
         />
         <div className="my-auto">
-          {latestTrack["@attr"]?.nowplaying !== undefined ? (
-            <p className="italic">Now Playing</p>
-          ) : null}
+          <div className="flex flex-row space-x-1 text-red-400 items-center">
+            <PiWaveformBold className="w-5 h-5" />
+            <p className="text-sm font-medium">{playingWhen}</p>
+          </div>
           <p className="font-pop font-semibold text-lg">{latestTrack.name}</p>
           <p className="font-default">{latestTrack.artist["#text"]}</p>
         </div>
@@ -36,5 +42,40 @@ const LastFmCard = () => {
     );
   }
 };
+
+function getTimeDiff(givenDate: string) {
+  const givenDatetime = new Date(`${givenDate} UTC`);
+  const currentDatetime = new Date();
+
+  // Calculate the time difference in milliseconds
+  const timeDifferenceInMilliseconds =
+    currentDatetime.valueOf() - givenDatetime.valueOf();
+
+  // Convert milliseconds to seconds
+  const timeDifferenceInSeconds = timeDifferenceInMilliseconds / 1000;
+
+  // Convert seconds to minutes, hours, days, etc. based on your requirements
+  const secondsInMinute = 60;
+  const secondsInHour = secondsInMinute * 60;
+  const secondsInDay = secondsInHour * 24;
+
+  const daysAgo = Math.floor(timeDifferenceInSeconds / secondsInDay);
+  const hoursAgo = Math.floor(
+    (timeDifferenceInSeconds % secondsInDay) / secondsInHour
+  );
+  const minutesAgo = Math.floor(
+    ((timeDifferenceInSeconds % secondsInDay) % secondsInHour) / secondsInMinute
+  );
+
+  console.log(daysAgo, hoursAgo, minutesAgo);
+
+  if (daysAgo === 0 && hoursAgo === 0) {
+    return `About ${minutesAgo} mins ago`;
+  } else if (daysAgo === 0) {
+    return `About ${hoursAgo} hours ago`;
+  } else {
+    return `About ${daysAgo} days ago`;
+  }
+}
 
 export default LastFmCard;
