24748cfee8b5ca4989303edbc91e1aa8d0742219
Zach Bauer <azbauer8@gmail.com>
2023-11-26 12:28:43 -0500

    hiding activies section til I get it working

--- components/ui/mode-toggle.tsx
+++ components/ui/mode-toggle.tsx
@@ -13,14 +13,14 @@ export function ModeToggle() {
     <Button
       variant="ghost"
       size="icon"
-      className="hover:bg-transparent focus-visible:ring-transparent dark:hover:bg-transparent dark:focus-visible:ring-transparent"
+      className="hover:bg-transparent focus-visible:ring-transparent dark:hover:bg-transparent dark:focus-visible:ring-transparent cursor-default"
     >
       <Sun
-        className="h-[1.5rem] w-[1.5rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0"
+        className="h-[1.5rem] w-[1.5rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0 cursor-pointer"
         onClick={() => setTheme("dark")}
       />
       <Moon
-        className="absolute h-[1.5rem] w-[1.5rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100"
+        className="absolute h-[1.5rem] w-[1.5rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100 cursor-pointer"
         onClick={() => setTheme("light")}
       />
       <span className="sr-only">Toggle theme</span>
