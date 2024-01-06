becfe4ee94d60bf691994b1f5a254f058ab118f4
Zach Bauer <azbauer8@gmail.com>
2024-01-06 15:23:41 -0500

    added vercel speed insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import Footer from "@/components/Footer";
 import Header from "@/components/Header";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 
@@ -9,28 +10,31 @@ import { Providers } from "./providers";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Zach Bauer",
-  description: `Zach Bauer's Personal Website`,
+	title: "Zach Bauer",
+	description: `Zach Bauer's Personal Website`,
 };
 
 export default function RootLayout({
-  children,
+	children,
 }: {
-  children: React.ReactNode;
+	children: React.ReactNode;
 }) {
-  return (
-    <html lang="en" suppressHydrationWarning>
-      <body className={`${inter.className}`}>
-        <Providers>
-          <div className="mx-auto max-w-3xl space-y-5">
-            <Header />
-            <div className="m-5 space-y-8 md:m-0">
-              <main>{children}</main>
-              <Footer />
-            </div>
-          </div>
-        </Providers>
-      </body>
-    </html>
-  );
+	return (
+		<html lang="en" suppressHydrationWarning>
+			<body className={`${inter.className}`}>
+				<Providers>
+					<div className="mx-auto max-w-3xl space-y-5">
+						<Header />
+						<div className="m-5 space-y-8 md:m-0">
+							<main>
+								{children}
+								<SpeedInsights />
+							</main>
+							<Footer />
+						</div>
+					</div>
+				</Providers>
+			</body>
+		</html>
+	);
 }
