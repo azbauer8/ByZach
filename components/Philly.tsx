c52cb8b577efc4f70821491449d4fd29dccd6990
Zach Bauer <azbauer8@gmail.com>
2024-01-23 19:51:00 -0500

    removed unused packages

--- components/Philly.tsx
+++ components/Philly.tsx
@@ -10,41 +10,41 @@ export default function Philly() {
     <TooltipProvider>
       <Tooltip delayDuration={0}>
         <TooltipTrigger>
-          <span className="group/korok inline-flex">
-            <span className="group/korok inline-flex cursor-birds decoration-zinc-150 decoration-2 underline-offset-2 hover:no-underline group-hover/korok:text-[#004C54] dark:decoration-zinc-450 md:text-zinc-800 md:underline dark:md:text-zinc-50">
-              <span className="sr-only">Philly</span>
+          <span className="group/philly inline-flex">
+            <span className="group/philly inline-flex cursor-birds select-none decoration-zinc-150 decoration-2 underline-offset-2 hover:no-underline group-hover/philly:text-[#004C54] dark:decoration-zinc-450 md:text-zinc-800 md:underline dark:md:text-zinc-50">
+              <span className="sr-only select-text">Philly</span>
               <span
-                className="transition delay-[50ms] duration-75 group-hover/korok:-translate-y-px"
+                className="transition delay-[50ms] duration-75 group-hover/philly:-translate-y-px"
                 aria-hidden="true"
               >
                 P
               </span>
               <span
-                className="transition delay-[75ms] duration-75 group-hover/korok:-translate-y-px"
+                className="transition delay-[75ms] duration-75 group-hover/philly:-translate-y-px"
                 aria-hidden="true"
               >
                 h
               </span>
               <span
-                className="transition delay-[100ms] duration-75 group-hover/korok:-translate-y-px"
+                className="transition delay-[100ms] duration-75 group-hover/philly:-translate-y-px"
                 aria-hidden="true"
               >
                 i
               </span>
               <span
-                className="transition delay-[125ms] duration-75 group-hover/korok:-translate-y-px"
+                className="transition delay-[125ms] duration-75 group-hover/philly:-translate-y-px"
                 aria-hidden="true"
               >
                 l
               </span>
               <span
-                className="transition delay-[150ms] duration-75 group-hover/korok:-translate-y-px"
+                className="transition delay-[150ms] duration-75 group-hover/philly:-translate-y-px"
                 aria-hidden="true"
               >
                 l
               </span>
               <span
-                className="transition delay-[200ms] duration-75 group-hover/korok:-translate-y-px"
+                className="transition delay-[200ms] duration-75 group-hover/philly:-translate-y-px"
                 aria-hidden="true"
               >
                 y
