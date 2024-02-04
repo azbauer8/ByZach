d3b265016aead360ee0656f79dfcc05a6ea436ed
Zach Bauer <azbauer8@gmail.com>
2024-02-03 19:02:39 -0500

    removing revalidation - increased load time substantially

--- src/app/sections/Activity/TraktCard.tsx
+++ src/app/sections/Activity/TraktCard.tsx
@@ -13,9 +13,6 @@ async function loader() {
 					"trakt-api-key": `${process.env.TRAKT_API}`,
 					"trakt-api-version": "2",
 				},
-				next: {
-					revalidate: 0,
-				},
 			},
 		)
 		const traktData = await response.json()
@@ -27,9 +24,6 @@ async function loader() {
 
 		const imdbData = await fetch(
 			`http://omdbapi.com/?apikey=${process.env.OMDB_API}&i=${imdbId}`,
-			{
-				next: { revalidate: 0 },
-			},
 		)
 
 		const { Poster } = await imdbData.json()
