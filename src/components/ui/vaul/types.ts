bbb801909ba508e351348076891c402ea4069242
Zach Bauer <azbauer8@gmail.com>
2024-04-14 17:04:47 -0400

    fixing drawer client error

--- src/components/ui/vaul/types.ts
+++
@@ -1,7 +0,0 @@
-// @ts-nocheck
-/* eslint-disable @typescript-eslint/no-empty-function */
-export type DrawerDirection = "top" | "bottom" | "left" | "right"
-export interface SnapPoint {
-  fraction: number
-  height: number
-}
