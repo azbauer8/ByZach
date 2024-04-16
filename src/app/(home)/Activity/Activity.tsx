429b8a69a73b029c41975721caa40c867b975675
Zach Bauer <azbauer8@gmail.com>
2024-04-15 21:59:37 -0400

    unnecessary div nesting

--- src/app/(home)/Activity/Activity.tsx
+++ src/app/(home)/Activity/Activity.tsx
@@ -9,12 +9,10 @@ import TraktCard from "./TraktCard"
 export default function Activity() {
   return (
     <div className="space-y-5">
-      <div className="space-y-0.5">
-        <Typography variant="h3">Activity</Typography>
-        <Typography variant="p" affects="muted">
-          I enjoy consuming media, sometimes.
-        </Typography>
-      </div>
+      <Typography variant="h3">Activity</Typography>
+      <Typography variant="p" affects="muted">
+        I enjoy consuming media, sometimes.
+      </Typography>
       <Suspense fallback={<LoadingLastFm />}>
         <LastFmCard />
       </Suspense>
