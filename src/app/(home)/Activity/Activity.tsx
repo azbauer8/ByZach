db97eff7143805e729a23a9fecc49c405ca57f0d
Zach Bauer <azbauer8@gmail.com>
2024-04-15 21:59:21 -0400

    removing hover styling from activity header

--- src/app/(home)/Activity/Activity.tsx
+++ src/app/(home)/Activity/Activity.tsx
@@ -10,12 +10,7 @@ export default function Activity() {
   return (
     <div className="space-y-5">
       <div className="space-y-0.5">
-        <Typography
-          variant="h3"
-          className="underline-offset-2 hover:text-primary hover:underline active:text-primary  active:underline"
-        >
-          Activity
-        </Typography>
+        <Typography variant="h3">Activity</Typography>
         <Typography variant="p" affects="muted">
           I enjoy consuming media, sometimes.
         </Typography>
