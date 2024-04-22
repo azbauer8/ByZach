7fb8d6653ebbdb9c54f3926d376e20d506c461b5
Zach Bauer <azbauer8@gmail.com>
2024-04-21 22:43:02 -0400

    removed unnecessary client/server boundaries

---
+++ src/app/uses/UsesTabs.tsx
@@ -0,0 +1,30 @@
+"use client"
+
+import Link from "next/link"
+import { usePathname } from "next/navigation"
+
+import { cn } from "@/lib/utils"
+
+export default function Tabs() {
+  return (
+    <div className="flex items-center rounded-lg bg-content1 p-1 text-sm">
+      <Tab title="Software" link="/uses" />
+      <Tab title="Hardware" link="/uses/hardware" />
+    </div>
+  )
+}
+
+function Tab({ title, link }: { title: string; link: string }) {
+  const path = usePathname()
+  return (
+    <Link
+      className={cn(
+        "flex-1 rounded-lg py-2 text-center font-medium text-default3",
+        path === link && "bg-background text-foreground"
+      )}
+      href={link}
+    >
+      {title}
+    </Link>
+  )
+}
---
+++ src/components/ContentList.tsx
@@ -0,0 +1,83 @@
+"use client"
+
+import { useState } from "react"
+import Link from "next/link"
+import { usePathname } from "next/navigation"
+
+import { capitalize, cn, formatDate } from "@/lib/utils"
+import { Text } from "@/components/ui/text"
+import { MobileSidebarToggle } from "@/components/Sidebar/MobileSidebarToggle"
+import { StickyHeader } from "@/components/StickyHeader"
+
+export default function ContentList({
+  id,
+  type,
+  links,
+}: {
+  id: string
+  type: "projects" | "discoveries" | "thoughts" | "uses" | "snippets"
+  links: {
+    slug: string
+    metadata: {
+      title: string
+      subtitle?: string
+      category?: string | null
+      dateTime?: string | null
+    }
+  }[]
+}) {
+  const path = usePathname()
+  const isContentPage = path.split("/").length === 3
+  const title = capitalize(type)
+
+  const [scroll, setScroll] = useState(0)
+  const onScroll = () => {
+    const scrollTop = document.getElementById(id)?.scrollTop
+    scrollTop && setScroll(scrollTop)
+  }
+  return (
+    <div
+      className={cn(
+        isContentPage
+          ? "absolute top-0 max-h-dvh min-h-dvh w-80 -translate-x-full overflow-y-auto border-r-[0.5px] bg-content1 xl:sticky xl:translate-x-0"
+          : "max-h-dvh min-h-dvh w-full overflow-y-auto xl:w-80 xl:border-r-[0.5px] xl:bg-content1"
+      )}
+      id={id}
+      onScroll={onScroll}
+    >
+      <StickyHeader title={title} scrollPos={scroll}>
+        <MobileSidebarToggle />
+      </StickyHeader>
+      <div className="flex flex-col gap-0.5 p-3 pt-0">
+        <Text variant="h3" className="pb-2 pl-1">
+          {title}
+        </Text>
+        {links.map((link) => {
+          const fullLink = `/${type}/${link.slug}`
+          const active = path === fullLink
+          return (
+            <Link
+              key={link.slug}
+              href={fullLink}
+              className={cn(
+                "size-full rounded-lg px-2  py-1.5  hover:bg-content2 active:bg-content2",
+                active && "bg-content2 text-primary"
+              )}
+            >
+              <Text className="font-medium">{link.metadata.title}</Text>
+              <Text className="text-default3">
+                {link.metadata.subtitle
+                  ? link.metadata.subtitle
+                  : link.metadata.category
+                    ? link.metadata.category
+                    : link.metadata.dateTime
+                      ? formatDate(link.metadata.dateTime)
+                      : undefined}
+              </Text>
+            </Link>
+          )
+        })}
+      </div>
+    </div>
+  )
+}
