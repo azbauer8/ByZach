b17a72f6513c9eec9a0fb4a7e12ee233486d21e8
Zach Bauer <azbauer8@gmail.com>
2024-04-23 18:19:20 -0400

    testing performance with ppr disabled

--- next.config.js
+++ next.config.js
@@ -1,13 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-
-  experimental: {
-    ppr: true
-  },
-
   transpilePackages: ['next-mdx-remote'],
-
+  modularizeImports: {
+    'react-icons/?(((\\w*)?/?)*)': {
+      transform: '@react-icons/all-files/{{ matches.[1] }}/{{ member }}',
+      skipDefaultConversion: true,
+    },
+  },
   images: {
     remotePatterns: [
       {
