a785714632677c0e0f466d84c8e7edd7abdcbcb9
Zach Bauer <azbauer8@gmail.com>
2024-04-24 20:39:19 -0400

    fixing imports in dev environment

---
+++ next.config.mjs
@@ -0,0 +1,37 @@
+const config = () => {
+  const env = process.env.NODE_ENV
+  /** @type {import('next').NextConfig} */
+  const nextConfig = {
+    experimental: {
+      webVitalsAttribution: ['FCP', 'LCP', 'CLS', 'FID', 'TTFB', 'INP'],
+    },
+    transpilePackages: ['next-mdx-remote'],
+
+    images: {
+      remotePatterns: [
+        {
+          protocol: "https",
+          hostname: "*",
+        },
+        {
+          protocol: "http",
+          hostname: "*",
+        },
+      ],
+    }
+  }
+  if (env === "development") {
+    return nextConfig
+  }
+  return {
+    ...nextConfig,
+    modularizeImports: {
+      'react-icons/?(((\\w*)?/?)*)': {
+        transform: '@react-icons/all-files/{{ matches.[1] }}/{{ member }}',
+        skipDefaultConversion: true,
+      },
+    },
+  }
+}
+
+export default config
