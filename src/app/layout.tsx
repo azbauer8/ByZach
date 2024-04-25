4bec02b13fa434325ebc0548949ab0f8bbe45d18
Zach Bauer <azbauer8@gmail.com>
2024-04-24 21:26:21 -0400

    added pre-commit linting

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,94 +1,94 @@
-import { siteConfig, siteLinks } from "@/config"
+import { siteConfig, siteLinks } from "@/config";
 
-import "@/styles/global.css"
+import "@/styles/global.css";
 
-import type { Metadata, Viewport } from "next"
-import { Asap } from "next/font/google"
-import { Analytics } from "@vercel/analytics/react"
-import { SpeedInsights } from "@vercel/speed-insights/next"
-import { GeistMono } from "geist/font/mono"
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
+import { GeistMono } from "geist/font/mono";
+import type { Metadata, Viewport } from "next";
+import { Asap } from "next/font/google";
 
-import { cn } from "@/lib/utils"
-import Providers from "@/components/Providers"
-import MobileSidebar from "@/components/Sidebar/MobileSidebar"
-import Sidebar from "@/components/Sidebar/Sidebar"
+import Providers from "@/components/Providers";
+import MobileSidebar from "@/components/Sidebar/MobileSidebar";
+import Sidebar from "@/components/Sidebar/Sidebar";
+import { cn } from "@/lib/utils";
 
 const asap = Asap({
-  subsets: ["latin"],
-  variable: "--font-sans",
-})
+	subsets: ["latin"],
+	variable: "--font-sans",
+});
 
 export const viewport: Viewport = {
-  themeColor: [
-    { media: "(prefers-color-scheme: light)", color: "#FEFEFE" },
-    { media: "(prefers-color-scheme: dark)", color: "#080808" },
-  ],
-}
+	themeColor: [
+		{ media: "(prefers-color-scheme: light)", color: "#FEFEFE" },
+		{ media: "(prefers-color-scheme: dark)", color: "#080808" },
+	],
+};
 
 export const metadata: Metadata = {
-  title: {
-    default: siteConfig.title,
-    template: `%s | ${siteConfig.title}`,
-  },
-  description: siteConfig.description,
-  applicationName: siteConfig.title,
-  icons: {
-    icon: siteConfig.favicon,
-    shortcut: siteConfig.favicon,
-  },
-  alternates: {
-    canonical: siteLinks.here,
-  },
-  metadataBase: new URL(siteLinks.here),
-  openGraph: {
-    title: siteConfig.title,
-    description: siteConfig.description,
-    url: siteLinks.here,
-    siteName: siteConfig.title,
-    locale: "en_US",
-    type: "website",
-  },
-  twitter: {
-    card: "summary_large_image",
-    title: siteConfig.title,
-    description: siteConfig.description,
-  },
-  robots: {
-    index: true,
-    follow: true,
-    googleBot: {
-      index: true,
-      follow: true,
-      "max-video-preview": -1,
-      "max-image-preview": "large",
-      "max-snippet": -1,
-    },
-  },
-}
+	title: {
+		default: siteConfig.title,
+		template: `%s | ${siteConfig.title}`,
+	},
+	description: siteConfig.description,
+	applicationName: siteConfig.title,
+	icons: {
+		icon: siteConfig.favicon,
+		shortcut: siteConfig.favicon,
+	},
+	alternates: {
+		canonical: siteLinks.here,
+	},
+	metadataBase: new URL(siteLinks.here),
+	openGraph: {
+		title: siteConfig.title,
+		description: siteConfig.description,
+		url: siteLinks.here,
+		siteName: siteConfig.title,
+		locale: "en_US",
+		type: "website",
+	},
+	twitter: {
+		card: "summary_large_image",
+		title: siteConfig.title,
+		description: siteConfig.description,
+	},
+	robots: {
+		index: true,
+		follow: true,
+		googleBot: {
+			index: true,
+			follow: true,
+			"max-video-preview": -1,
+			"max-image-preview": "large",
+			"max-snippet": -1,
+		},
+	},
+};
 
 export default function RootLayout({
-  children,
+	children,
 }: {
-  children: React.ReactNode
+	children: React.ReactNode;
 }) {
-  return (
-    <html lang="en" suppressHydrationWarning>
-      <head />
-      <body
-        className={cn(
-          "flex bg-background font-sans text-foreground antialiased",
-          asap.variable,
-          GeistMono.variable
-        )}
-      >
-        <Providers>
-          <Sidebar />
-          <MobileSidebar />
-          <div className="flex-1">{children}</div>
-        </Providers>
-        <Analytics />
-        <SpeedInsights />
-      </body>
-    </html>
-  )
+	return (
+		<html lang="en" suppressHydrationWarning>
+			<head />
+			<body
+				className={cn(
+					"flex bg-background font-sans text-foreground antialiased",
+					asap.variable,
+					GeistMono.variable,
+				)}
+			>
+				<Providers>
+					<Sidebar />
+					<MobileSidebar />
+					<div className="flex-1">{children}</div>
+				</Providers>
+				<Analytics />
+				<SpeedInsights />
+			</body>
+		</html>
+	);
 }
