a6011e8416aef7559ea5abaea266129722d3e2c5
Zach Bauer <azbauer8@gmail.com>
2024-04-24 20:27:42 -0400

    removing million - seems to be causing hydration issues

--- next.config.js
+++ next.config.js
@@ -1,17 +1,15 @@
-import million from 'million/compiler'
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
     webVitalsAttribution: ['FCP', 'LCP', 'CLS', 'FID', 'TTFB', 'INP'],
   },
   transpilePackages: ['next-mdx-remote'],
-  // modularizeImports: {
-  //   'react-icons/?(((\\w*)?/?)*)': {
-  //     transform: '@react-icons/all-files/{{ matches.[1] }}/{{ member }}',
-  //     skipDefaultConversion: true,
-  //   },
-  // },
+  modularizeImports: {
+    'react-icons/?(((\\w*)?/?)*)': {
+      transform: '@react-icons/all-files/{{ matches.[1] }}/{{ member }}',
+      skipDefaultConversion: true,
+    },
+  },
   images: {
     remotePatterns: [
       {
@@ -26,12 +24,4 @@ const nextConfig = {
   }
 }
 
-const millionConfig = {
-  auto: {
-    rsc: true
-  },
-  server: true,
-  rsc: true
-}
-
-export default million.next(nextConfig, millionConfig)
+export default nextConfig
